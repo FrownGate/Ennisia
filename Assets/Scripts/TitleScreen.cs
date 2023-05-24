7b6de3d24b66ad0f43968d6478bbc03628f52558
Emilie Gonin <emilie.yuu@gmail.com>
2023-05-24 15:37:11 +0200

    :sparkles: Optimize TitleScreen
    • Replace button with a box collider & on mouse down function on the background panel

--- Assets/Scripts/TitleScreen.cs
+++ Assets/Scripts/TitleScreen.cs
@@ -1,31 +1,10 @@
-using System;
 using UnityEngine;
-using UnityEngine.UI;
-using System.Collections;
 using UnityEngine.SceneManagement;
 
-namespace DefaultNamespace
+public class TitleScreen : MonoBehaviour
 {
-    public class TitleScreen : MonoBehaviour
+    private void OnMouseDown()
     {
-        public Button Start;
-
-
-        private void OnEnable()
-        {
-            //Register Button Events
-            Start.onClick.AddListener(() => buttonCallBack(Start));
-
-        }
-
-
-        private void buttonCallBack(Button buttonPressed)
-        {
-            if (buttonPressed == Start)
-            {
-                SceneManager.LoadScene("MainMenu");
-                Debug.Log("Clicked: " + Start.name);
-            }  
-        }
+        SceneManager.LoadScene("MainMenu");
     }
 }
