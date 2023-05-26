5fd556b12edddadbd72020f92b9b9f36dd6027cb
Valentin Faguet <valentin@wallmatt.com>
2023-05-26 17:28:01 +0200

    :star: Init UI

--- Assets/Scripts/BattleLoop/BattleSimulator.cs
+++ Assets/Scripts/BattleLoop/BattleSimulator.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using UnityEngine.UIElements;
+using System.IO;
+
 
 public class BattleSimulator : EditorWindow
 {
+
+    private List<string> _enemies = new();
     [MenuItem("Tools/Battle Simulator")]
     public static void ShowWindow()
     {
@@ -17,6 +21,117 @@ public class BattleSimulator : EditorWindow
         GetWindow<BattleSimulator>("Battle Simulator");
         BattleSimulator window = GetWindow<BattleSimulator>("Battle Simulator");
         window.titleContent = new GUIContent("Battle Simulator");
-        window.position = new Rect(Screen.width / 2, Screen.height / 2, 500, 500);
+        window.position = new Rect(Screen.width / 2, Screen.height / 2, 700, 700);
     }
+
+
+    private void OnGUI()
+    {
+        // create random enemies for testing
+        for (int i = 0; i < 10; i++)
+        {
+            _enemies.Add($"Enemy {i}");
+        }
+        rootVisualElement.Clear();
+        VisualElement main = rootVisualElement;
+
+
+        float splitPosition = 0.5f; // Split position as a normalized value (0 to 1)
+
+        float splitPixelPosition = splitPosition * Screen.width;
+
+        // LEFT PANEL
+        GUI.BeginGroup(new Rect(0, 0, splitPixelPosition, Screen.height));
+        GUI.Box(new Rect(0, 0, splitPixelPosition, Screen.height), "");
+        // Add left panel contents here
+
+        AlliesGUI();
+
+        // simulateButton.clicked += () => { Debug.Log("Simulate"); };
+        GUI.EndGroup();
+
+        // RIGHT PANEL
+        GUI.BeginGroup(new Rect(splitPixelPosition, 0, Screen.width - splitPixelPosition, Screen.height));
+        GUI.Box(new Rect(0, 0, Screen.width - splitPixelPosition, Screen.height), "");
+        // Add right panel contents here
+
+        EnemiesGUI();
+
+        UpdateEnemySelectionGUI();
+        GUI.EndGroup();
+
+
+    }
+
+    private void AlliesGUI()
+    {
+        // EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+
+        // create guilayout label centered
+        GUILayout.Label("Allies",
+           EditorStyles.boldLabel,
+           GUILayout.Width(200),
+           GUILayout.Height(30),
+           GUILayout.ExpandWidth(true),
+           GUILayout.ExpandHeight(false)
+           );
+
+        // EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+
+
+    }
+
+    private void EnemiesGUI()
+    {
+        // EditorGUILayout.BeginHorizontal();
+        EditorGUILayout.BeginVertical();
+        GUILayout.Label("Enemies",
+           EditorStyles.boldLabel,
+           GUILayout.Width(200),
+           GUILayout.Height(30),
+           GUILayout.ExpandWidth(true),
+           GUILayout.ExpandHeight(false)
+           );
+        // EditorGUILayout.EndHorizontal();
+        EditorGUILayout.EndVertical();
+
+    }
+
+    private void UpdateEnemySelectionGUI()
+    {
+
+        EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+
+
+        EditorGUILayout.LabelField("Enemies", EditorStyles.boldLabel);
+
+        // foreach (string enemy in _enemies)
+        // {
+        //     Button enemyButton = new()
+        //     {
+        //         text = enemy,
+        //         style =
+        //         {
+        //             width = 100,
+        //             height = 30,
+        //             marginBottom = 30,
+        //             marginTop = 15,
+        //             paddingLeft = 5,
+        //             paddingRight = 5,
+        //             paddingTop = 5,
+        //             paddingBottom = 5,
+        //             alignItems = Align.Center,
+        //             justifyContent = Justify.Center,
+        //         },
+        //     };
+        //     _rightPane.Add(enemyButton);
+        // }
+
+
+        EditorGUILayout.EndVertical();
+
+    }
+
 }
