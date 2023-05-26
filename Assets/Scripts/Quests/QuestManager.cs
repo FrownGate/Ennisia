f223e1ecc72356eda2da42deea99786336151e83
Crimson41 <alexis.plp41@gmail.com>
2023-05-26 15:28:37 +0200

    ✨ Rebuild all script

--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -17,6 +17,9 @@ public class QuestManager : MonoBehaviour
     private const float dailyQuestCooldownDuration = 24f * 60f * 60f; // Durée du cooldown pour la quête quotidienne en secondes
     private const float weeklyQuestCooldownDuration = 7f * 24f * 60f * 60f; // Durée du cooldown pour la quête hebdomadaire en secondes
 
+    private float dailyQuestTimer;
+    private float weeklyQuestTimer;
+
     private void Start()
     {
         dailyButtonPressCount = 0;
@@ -27,6 +30,22 @@ public class QuestManager : MonoBehaviour
 
         lastDailyQuestCompletionTime = PlayerPrefs.GetFloat("LastDailyQuestCompletionTime", 0f);
         lastWeeklyQuestCompletionTime = PlayerPrefs.GetFloat("LastWeeklyQuestCompletionTime", 0f);
+
+        dailyQuestTimer = 0f;
+        weeklyQuestTimer = 0f;
+    }
+
+    private void Update()
+    {
+        if (!dailyQuestCompleted && CanCompleteDailyQuest())
+        {
+            Debug.Log("Daily quest is ready to be completed!");
+        }
+
+        if (!weeklyQuestCompleted && CanCompleteWeeklyQuest())
+        {
+            Debug.Log("Weekly quest is ready to be completed!");
+        }
     }
 
     public void OnButtonPress()
@@ -55,6 +74,25 @@ public class QuestManager : MonoBehaviour
         return Time.time >= lastWeeklyQuestCompletionTime + weeklyQuestCooldownDuration;
     }
 
+    private float GetTimeRemainingForDailyQuest()
+    {
+        return Mathf.Max(0f, lastDailyQuestCompletionTime + dailyQuestCooldownDuration - Time.time);
+    }
+
+    private float GetTimeRemainingForWeeklyQuest()
+    {
+        return Mathf.Max(0f, lastWeeklyQuestCompletionTime + weeklyQuestCooldownDuration - Time.time);
+    }
+
+    private string FormatTime(float time)
+    {
+        int hours = Mathf.FloorToInt(time / 3600);
+        int minutes = Mathf.FloorToInt((time % 3600) / 60);
+        int seconds = Mathf.FloorToInt(time % 60);
+
+        return string.Format("{0:00}:{1:00}:{2:00}", hours, minutes, seconds);
+    }
+
     private void CompleteDailyQuest()
     {
         dailyQuestCompleted = true;
@@ -63,7 +101,8 @@ public class QuestManager : MonoBehaviour
         Debug.Log("Daily quest completed!");
 
         // Ajoutez ici le code pour récompenser le joueur pour la quête quotidienne
-        // Réinitialisez également le compteur de pressions quotidiennes et mettez à jour l'état de la quête
+
+        // Initialiser le compteur de pressions quotidiennes
         dailyButtonPressCount = 0;
     }
 
@@ -75,7 +114,8 @@ public class QuestManager : MonoBehaviour
         Debug.Log("Weekly quest completed!");
 
         // Ajoutez ici le code pour récompenser le joueur pour la quête hebdomadaire
-        // Réinitialisez également le compteur de pressions hebdomadaires et mettez à jour l'état de la quête
+
+        // Initialiser le compteur de pressions hebdomadaires
         weeklyButtonPressCount = 0;
     }
 }
