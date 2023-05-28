32a9dd000a79e71decdd6d73c2f54b9709e10be2
Emilie Gonin <emilie.yuu@gmail.com>
2023-05-28 17:23:48 +0200

    :star: Link StuffMenu with EquippedGears

--- Assets/Scripts/SO/EquipmentSO.cs
+++ Assets/Scripts/SO/EquipmentSO.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 [CreateAssetMenu(fileName = "NewEquipment", menuName = "Equipment/New")]
@@ -11,4 +12,14 @@ public class EquipmentSO : ScriptableObject
     public float value;
     public string description;
     public Sprite Icon;
+
+    public int TypeIndex()
+    {
+        Dictionary<string, int> types = new()
+        {
+            { "Helmet", 0 }, { "Chest", 1 }, { "Boots", 2 }, { "Earrings", 3 }, { "Necklace", 4 }, { "Ring", 5 }
+        };
+
+        return types[type];
+    }
 }
