32d7eb566d3799f82d10c3c69b868f11c2f9fb0d
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2023-05-30 15:38:29 +0200

    :star: Weapon script

---
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public string weaponName;
+    public bool isMagic;
+    public Skill skill1;
+    public Skill skill2;
+    public TidalCrash tidalCrash;
+
+
+    /*string filename = "TidalCrash";
+    Type type = Type.GetType(filename);
+    Skill skill = (Skill)Activator.CreateInstance(type);*/
+}
