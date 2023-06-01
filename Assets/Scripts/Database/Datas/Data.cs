5a342dff9779b699535ddd054b31905b88b0b71f
Emilie Gonin <emilie.yuu@gmail.com>
2023-06-01 17:48:11 +0200

    :sparkles: Optimize getting data classname

--- Assets/Scripts/Database/Datas/Data.cs
+++ Assets/Scripts/Database/Datas/Data.cs
@@ -9,10 +9,15 @@ public abstract class Data
     {
         return new SetObject
         {
-            ObjectName = GetType().Name.Replace("Data", string.Empty),
+            ObjectName = GetName(),
             EscapedDataObject = JsonUtility.ToJson(this)
         };
     }
 
     public abstract void UpdateLocalData(string json);
+
+    public string GetName()
+    {
+        return GetType().Name.Replace("Data", string.Empty);
+    }
 }
