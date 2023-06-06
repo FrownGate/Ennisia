fee5f49e62f0d6f1bcd27fa23f2b70ef37867d25
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2023-06-06 16:40:47 +0200

    :star: weekly quests added

--- Assets/Scripts/Quests/WeekliesCheck.cs
+++ Assets/Scripts/Quests/WeekliesCheck.cs
@@ -3,12 +3,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DailiesCheck : MonoBehaviour
+public class WeekliesCheck : MonoBehaviour
 {
-    public static event Action daily1;
-    public static event Action daily2;
-    public static event Action daily3;
-    public static event Action daily4;
+    public static event Action weekly1;
+    public static event Action weekly2;
+    public static event Action weekly3;
+    public static event Action weekly4;
     public int dungeonNeeded;
     public int mainStoryNeeded;
     public int goldAmount;
