efbda76f2af73fc9ad0d806528ad66c90d00849c
Ever <everfrithza@gmail.com>
2023-06-09 12:56:25 +0200

    :star: :rainbow: fuse gearSO and WeaponSO
    also add things for the skills of supports, missing csv

--- Assets/Scripts/SO/GearSO.cs
+++ Assets/Scripts/SO/GearSO.cs
@@ -16,7 +16,20 @@ public class GearSO : ScriptableObject
     //public Sprite Icon;
     public Dictionary<Item.AttributeStat, float> Substats;
 
-    
+    //Weapons
+    public bool IsMagic;
+    public SkillSO FirstSkillData;
+    public SkillSO SecondSkillData;
+    [HideInInspector] public Skill FirstSkill;
+    [HideInInspector] public Skill SecondSkill;
+
+    public void Init()
+    {
+        Type type = System.Type.GetType(CSVUtils.GetFileName(FirstSkillData.Name));
+        FirstSkill = (Skill)Activator.CreateInstance(type);
+        type = System.Type.GetType(CSVUtils.GetFileName(SecondSkillData.Name));
+        SecondSkill = (Skill)Activator.CreateInstance(type);
+    }
 
     public void Unequip()
     {
