be5e9b227b96a1278e060eff66a34c6adec857b4
Emilie Gonin <emilie.yuu@gmail.com>
2023-06-15 12:46:12 +0200

    :sparkles: Clean code

--- Assets/Scripts/ChangeTips.cs
+++ Assets/Scripts/ChangeTips.cs
@@ -1,19 +1,15 @@
-using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 
 public class ChangeTips : MonoBehaviour
 {
-
-
-    [SerializeField] public List<string> tips;
+    //TODO -> use CSV instead of serialize field
+    [SerializeField] private List<string> _tips;
     [SerializeField] private TextMeshProUGUI _tip;
-    // Start is called before the first frame update
+
     void Start()
     {
-        _tip.text = tips[Random.Range(1, tips.Count)];
+        _tip.text = _tips[Random.Range(1, _tips.Count)];
     }
-
-
 }
--- Assets/Scripts/SceneButton.cs
+++ Assets/Scripts/SceneButton.cs
@@ -3,11 +3,10 @@ using UnityEngine;
 
 public class SceneButton : MonoBehaviour
 {
-    [Scene]
-    public string Scene;
+    [Scene] public string Scene;
+
     private void OnMouseDown()
     {
-
         ScenesManager.Instance.SetScene(Scene); 
     }
 }
