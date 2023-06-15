1fc67f73bf193b61bb48d5caf5463d8e37bbcf22
Jun <jzhi@gaming.tech>
2023-06-15 16:21:24 +0200

    :star: Add MessageLogHandler

---
+++ Assets/Scripts/LogHandler.cs
@@ -0,0 +1,44 @@
+﻿
+    using System;
+    using TMPro;
+    using UnityEngine;
+    using Object = UnityEngine.Object;
+
+    public class LogHandler : MonoBehaviour, ILogHandler
+    {
+        [SerializeField] TextMeshProUGUI _promptText;
+        public static LogHandler Instance { get; private set; }
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+        private void Update()
+        {
+            LogException(new Exception(GetLogMessage()),this);
+        }
+        
+        public void LogFormat(LogType logType, Object context, string format, params object[] args)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void LogException(Exception exception, Object context)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetLogMessage()
+        {
+            string msg = "";
+            
+            return msg;
+        }
+    }
