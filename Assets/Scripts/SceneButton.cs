e833efa284a530c1fd849bb15a99f3a462845d24
Ever <everfrithza@gmail.com>
2023-06-15 15:13:29 +0200

    :star: End Mission Scene
    first Vers

--- Assets/Scripts/SceneButton.cs
+++ Assets/Scripts/SceneButton.cs
@@ -1,10 +1,16 @@
 using NaughtyAttributes;
 using UnityEngine;
-
+[RequireComponent(typeof(BoxCollider2D))]
 public class SceneButton : MonoBehaviour
 {
     [Scene]
     public string Scene;
+
+    private void Start()
+    {
+        BoxCollider2D box = GetComponent<BoxCollider2D>();
+        box.size = GetComponent<RectTransform>().sizeDelta;
+    }
     private void OnMouseDown()
     {
 
