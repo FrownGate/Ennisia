faab0dc0374be6eac3f3b23e957a624975f5361c
Hugo Caroumbin | "RatonFuté <hcaroumbin@gaming.tech>
2023-06-19 16:12:40 +0200

    :star: Gear set check

--- Assets/Scripts/Skills/List/Reinforcement.cs
+++ Assets/Scripts/Skills/List/Reinforcement.cs
@@ -6,7 +6,7 @@ public class Reinforcement : Skill
     public override void ConstantPassive(List<Entity> target, Entity player, int turn)
     {
         float defRatio = defBaseRatio + StatUpgrade1 * Level;
-        float PhdefBuff = player.Stats[Item.AttributeStat.Defense].Value * defRatio;
+        float PhdefBuff = player.Stats[Item.AttributeStat.PhysicalDefense].Value * defRatio;
         float MdefBuff = player.Stats[Item.AttributeStat.MagicalDefense].Value * defRatio;
         /*player.MagicDef = MdefBuff;
         player.PhysDef = PhdefBuff;*/ // modifier
