d84c874518a443cd635b4352da1b63f3359c47cd
Emilie Gonin <emilie.yuu@gmail.com>
2023-06-30 14:52:15 +0200

    :rainbow: Fix all errors

--- Assets/Scripts/Entities/Enemy.cs
+++ Assets/Scripts/Entities/Enemy.cs
@@ -4,65 +4,18 @@ public class Enemy : Entity
 {
     public Enemy()
     {
-        /*        Attack = 20;
-                MaxHp = 5000;
-                Level = 10;
-                Speed = 200;*/
-        Name = "betala";
+        Name = "Betala";
         Stats[Item.AttributeStat.Speed] = new(40);
         CurrentHp = Stats[Item.AttributeStat.HP].Value / 2;
 
     }
 
-    public Enemy(int id, string name, Dictionary<string, int> stats, string description)
+    public Enemy(int id, string name, Dictionary<Item.AttributeStat, float> stats, string description) : base(stats)
     {
         Id = id;
         Name = name;
         Description = description;
         Level = 1;
-
-        
-      /*  foreach (KeyValuePair<string, int> stat in stats)
-        {
-            switch (stat.Key) //TODO -> Use enum instead of strings
-            {
-                case "Hp":
-                    MaxHp = stat.Value;
-                    break;
-                case "Atk":
-                    Attack = stat.Value;
-                    break;
-                case "PhysAtk":
-                    PhysAtk = stat.Value;
-                    break;
-                case "MagicAtk":
-                    MagicAtk = stat.Value;
-                    break;
-                case "PhysDef":
-                    PhysDef = stat.Value;
-                    break;
-                case "MagicDef":
-                    MagicDef = stat.Value;
-                    break;
-                case "CritRate":
-                    CritRate = stat.Value;
-                    break;
-                case "CritDamage":
-                    CritDamage = stat.Value;
-                    break;
-                case "DefIgnored":
-                    DefIgnored = stat.Value;
-                    break;
-                case "Shield":
-                    Shield = stat.Value;
-                    break;
-                case "Speed":
-                    Speed = stat.Value;
-                    break;
-                default:
-                    break;
-            }
-        }*/
     }
 
     public override void HaveBeenSelected()
