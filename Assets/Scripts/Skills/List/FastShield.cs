6ca4844839afc9223fc5b4d5b27d728f098934fc
Tom LE GROS <freezekaz.dement@gmail.com>
2023-06-30 17:55:12 +0200

    :star: Add player IA and some functions

--- Assets/Scripts/Skills/List/FastShield.cs
+++ Assets/Scripts/Skills/List/FastShield.cs
@@ -4,7 +4,7 @@ public class FastShield : PassiveSkill
 {
     //TODO -> After an enemy attack, gives a shield, the shield strength is equivalent to 20% of the damage received  
 
-    public override void UseIfAttacked(List<Entity> targets, Entity player, int turn, float damageTaken)
+    public override void UseIfAttacked(List<Entity> targets,Entity caster, Entity player, int turn, float damageTaken)
     {
         float shieldAmount = damageTaken * Data.ShieldAmount / 100;
         player.Shield += shieldAmount;
