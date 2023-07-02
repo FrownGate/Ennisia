85193ca2512f501941435cb34324d03a0a0145d6
Emilie Gonin <emilie.yuu@gmail.com>
2023-07-02 19:08:43 +0200

    :sparkles: Clean tips

--- Assets/Scripts/ChangeTips.cs
+++ Assets/Scripts/ChangeTips.cs
@@ -1,23 +1,16 @@
 using UnityEngine;
-using UnityEditor;
 using System.IO;
 using System.Collections.Generic;
-using System;
-using System.Text.RegularExpressions;
-using System.Linq;
 using TMPro;
 
 public class ChangeTips : MonoBehaviour
 {
-
-
     [SerializeField] private TextMeshProUGUI _tip;
     private static int _currentLine;
     private static int _lines;
-    void Start()
+
+    private void Start()
     {
-
-
         string filePath = Application.dataPath + $"/Resources/CSV/Tips.csv";
         string[] lines = File.ReadAllLines(filePath);
         _lines = lines.Length;
@@ -29,13 +22,15 @@ public class ChangeTips : MonoBehaviour
         }
 
         Dictionary<int, string> rowData = new();
+
         for (int i = 1; i < lines.Length; i++)
         {
             _currentLine = i + 1;
             string[] values = CSVUtils.SplitCSVLine(lines[i]);
             rowData[int.Parse(values[0])] = values[1];
-            }
-        int random = UnityEngine.Random.Range(1, lines.Length);
+        }
+
+        int random = Random.Range(1, lines.Length);
         _tip.text = rowData[random];
     }
 }
