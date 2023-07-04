64c9dfff661137cd708f7ef3df5478b36c085659
Ever <everfrithza@gmail.com>
2023-07-04 16:21:23 +0200

    :rainbow: fix Raids button size

--- Assets/Scripts/UI/DynamicButtonGenerator.cs
+++ Assets/Scripts/UI/DynamicButtonGenerator.cs
@@ -9,23 +9,45 @@ public class DynamicButtonGenerator : MonoBehaviour
 
     private RectTransform _contentRect;
     private VerticalLayoutGroup _layoutGroup;
-
-
+    public float paddingPercentage = 0.03f;
     public List<GameObject> GenerateButtonsInSlider(int numberOfButtons)
     {
         List<GameObject> buttonsCreated = new();
         _contentRect = GetComponent<ScrollRect>().content;
         _layoutGroup = _contentRect.GetComponentInChildren<VerticalLayoutGroup>();
+
         for (int i = 0; i < numberOfButtons; i++)
         {
             GameObject button = Instantiate(ButtonPrefab, _contentRect);
             buttonsCreated.Add(button);
+
+            RectTransform buttonRect = button.GetComponent<RectTransform>();
+            
+
+            // Get the original width and height of the button
+            float originalWidth = buttonRect.sizeDelta.x;
+            float originalHeight = buttonRect.sizeDelta.y;
+
+            // Calculate the adjusted width based on the content width
+            float adjustedWidth = _contentRect.rect.width * (1f - paddingPercentage * 2f);
+            float ratio = adjustedWidth / originalWidth;
+            float adjustedHeight = originalHeight * ratio;
+
+            // Set the adjusted size
+            buttonRect.sizeDelta = new Vector2(adjustedWidth, adjustedHeight);
+
+            // Adjust the BoxCollider component
+            BoxCollider2D boxCollider = button.GetComponent<BoxCollider2D>();
+            if (boxCollider != null)
+            {
+                boxCollider.size = new Vector3(adjustedWidth, adjustedHeight);
+            }
         }
 
         // Adjust the content size to fit all buttons
         float totalHeight = CalculateTotalHeight(buttonsCreated);
         _contentRect.sizeDelta = new Vector2(_contentRect.sizeDelta.x, totalHeight);
-      //  GetComponentInChildren<Scrollbar>().value = 1;
+
         return buttonsCreated;
     }
 
