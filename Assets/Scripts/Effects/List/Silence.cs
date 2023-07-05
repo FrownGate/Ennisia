d0d5f608487251b8597cecd1ae87e8eb2981f6bd
Valentin Faguet <valentin@wallmatt.com>
2023-07-05 17:29:09 +0200

    :star: Add Silence AlterationEffect logic

--- Assets/Scripts/Effects/List/Silence.cs
+++ Assets/Scripts/Effects/List/Silence.cs
@@ -2,6 +2,15 @@ public class Silence : Effect
 {
     public override void AlterationEffect(Entity target)
     {
-        //
+        if (target is Player player)
+        {
+            player.Weapon.Skills[0].SkillButton.interactable = false;
+            player.Weapon.Skills[1].SkillButton.interactable = false;
+        }
+        else if (target is Enemy enemy)
+        {
+            enemy.Skills[1].SkillButton.interactable = false;
+            enemy.Skills[2].SkillButton.interactable = false;
+        }
     }
 }
