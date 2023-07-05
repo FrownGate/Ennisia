7be6bf4dc15886933dd4cf850ca3115423cabbcc
Jimmy Dutto <108483685+Kelorastus@users.noreply.github.com>
2023-07-05 18:13:46 +0200

    Added damages and buffs to the last enemy skills

--- Assets/Scripts/Skills/List/MalevolentStrike.cs
+++ Assets/Scripts/Skills/List/MalevolentStrike.cs
@@ -4,6 +4,17 @@ public class MalevolentStrike : PassiveSkill
 {
     public override void PassiveBeforeAttack(List<Entity> targets, Entity caster, int turn)
     {
-        // if atk buff, penetrate defense by 10%
+        foreach (Effect effect in caster.Effects)
+        {
+            if (effect.GetType() == typeof(AttackBuff))
+            {
+                targets[0].DefIgnored += 10;
+            }
+        }
+    }
+
+    public override void PassiveAfterAttack(List<Entity> targets, Entity caster, int turn, float damage)
+    {
+        targets[0].DefIgnored -= 10;
     }
 }
