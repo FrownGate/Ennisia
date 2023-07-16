8fbb61c54e4fd9a2a3564e1e3a8f6bb1497d7bbf
Emilie Gonin <emilie.yuu@gmail.com>
2023-07-16 15:45:43 +0200

    :sparkles: Clean ShowAccountSettings

--- Assets/Scripts/Database/Modules/Account/ShowAccountSettings.cs
+++ Assets/Scripts/Database/Modules/Account/ShowAccountSettings.cs
@@ -1,14 +1,11 @@
-using System;
 using UnityEngine;
 
 public class ShowAccountSettings : MonoBehaviour
 {
-    public static event Action<string> OnPetClick;
-
     [SerializeField] private GameObject _accountSettings;
     [SerializeField] private GameObject _gameSettings;
 
-    public void OnMouseDown()
+    public void OnMouseUpAsButton()
     {
         _accountSettings.SetActive(true);
         _gameSettings.SetActive(false);
