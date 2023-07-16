460c09dad099d1a9572063439fe4501da9e79df3
Emilie Gonin <emilie.yuu@gmail.com>
2023-07-16 16:42:33 +0200

    :sparkles: Clean settings

--- Assets/Scripts/UI Scripts/ShowGameSettings.cs
+++ Assets/Scripts/UI Scripts/ShowGameSettings.cs
@@ -1,5 +1,3 @@
-using System;
-using TMPro;
 using UnityEngine;
 
 public class ShowGameSettings : MonoBehaviour
@@ -7,7 +5,7 @@ public class ShowGameSettings : MonoBehaviour
     [SerializeField] private GameObject _accountSettings;
     [SerializeField] private GameObject _gameSettings;
 
-    public void OnMouseDown()
+    public void OnMouseUpAsButton()
     {
         _accountSettings.SetActive(false);
         _gameSettings.SetActive(true);
