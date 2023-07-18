c50b4674554425b0707ecd9f089a5c39e268ae45
Yoann Rocher <108529087+Everfrithza@users.noreply.github.com>
2023-07-18 15:18:22 +0200

    :rainbow: fix csv enemies

--- Assets/Scripts/Quests/QuestManager.cs
+++ Assets/Scripts/Quests/QuestManager.cs
@@ -9,7 +9,6 @@ public class QuestManager : MonoBehaviour
     public List<QuestSO> Daily;
     public List<QuestSO> Weekly;
 
-    public List<QuestSO> CurrentQuest;
     public List<QuestSO> AllQuests;
 
     public static QuestManager Instance { get; private set; }
