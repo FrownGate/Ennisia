6fdc76dce964c9e114a701cd3bacaf09dacbf611
Ever <everfrithza@gmail.com>
2023-07-31 12:57:59 +0200

    :star: change quest scene

--- Assets/Scripts/Quests/ShowQuests.cs
+++ Assets/Scripts/Quests/ShowQuests.cs
@@ -1,10 +1,7 @@
-using System;
-using PlayFab.GroupsModels;
 using System.Collections.Generic;
 using System.Linq;
 using TMPro;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 [RequireComponent(typeof(DynamicButtonGenerator))]
@@ -14,6 +11,8 @@ public class ShowQuests : MonoBehaviour
     private List<GameObject> _buttons;
     private List<QuestSO> _quests;
 
+    [SerializeField] private Slider _questTotalCompletionSlider;
+    
     private void Start()
     {
         _generator = GetComponent<DynamicButtonGenerator>();
@@ -44,11 +43,20 @@ public class ShowQuests : MonoBehaviour
         if (_buttons != null)
             foreach (var button in _buttons)
                 Destroy(button);
-        _buttons = _generator.GenerateButtonsInSlider(sortedQuests.Count);
+
+        // Split quests into three groups based on status
+        var completedQuests = sortedQuests.Where(quest => quest.Completed && !quest.RewardGiven).ToList();
+        var inProgressQuests = sortedQuests.Where(quest => !quest.Completed && !quest.RewardGiven).ToList();
+        var rewardGivenQuests = sortedQuests.Where(quest => quest.RewardGiven).ToList();
+
+        // Concatenate the three groups in the desired order
+        var orderedQuests = completedQuests.Concat(inProgressQuests).Concat(rewardGivenQuests).ToList();
+
+        _buttons = _generator.GenerateButtonsInSlider(orderedQuests.Count);
         var buttonIndex = 0;
         foreach (var go in _buttons)
         {
-            var quest = sortedQuests[buttonIndex];
+            var quest = orderedQuests[buttonIndex];
             go.name = quest.name;
 
             var questText = go.GetComponentInChildren<TextMeshProUGUI>();
@@ -60,21 +68,40 @@ public class ShowQuests : MonoBehaviour
             var questGoal = questDescription.transform.GetChild(0).GetComponentInChildren<TextMeshProUGUI>();
             foreach (var goal in quest.Goals) questGoal.text = $"{goal.CurrentAmount} / {goal.RequiredAmount}";
 
-            if (!quest.RewardGiven)
+            if (quest.RewardGiven || !quest.Completed) // Hide the button if the quest is not completed and the reward is given
             {
                 var finishBtn = go.GetComponentInChildren<Button>();
-                finishBtn.onClick.AddListener(() => OnClickButton(quest, go));
-                if (quest.Completed) finishBtn.interactable = true;
+                finishBtn.gameObject.SetActive(false);
+                if (quest.RewardGiven)
+                {
+                    go.GetComponent<Image>().color = new Color(0.39f, 0.39f, 0.39f);
+                }
             }
             else
             {
-                var bg = go.GetComponent<Image>();
-                bg.color = new Color(0.63f, 0.63f, 0.63f);
+                var finishBtn = go.GetComponentInChildren<Button>();
+                finishBtn.onClick.AddListener(() => OnClickButton(quest, go));
+                finishBtn.interactable = true;
             }
+            
+            // Calculate the quest completion percentage
+            float completionPercentage = 0f;
+            if (quest.Goals.Count > 0)
+            {
+                float totalRequiredAmount = quest.Goals.Sum(goal => goal.RequiredAmount);
+                float totalCurrentAmount = quest.Goals.Sum(goal => goal.CurrentAmount);
+                completionPercentage = totalCurrentAmount / totalRequiredAmount;
+            }
+
+            // Set the slider value
+            var completionSlider = go.GetComponentInChildren<Slider>();
+            if (completionSlider != null)
+                completionSlider.value = completionPercentage;
 
             buttonIndex++;
         }
     }
+
 
     private void OnClickButton(QuestSO quest, GameObject go)
     {
