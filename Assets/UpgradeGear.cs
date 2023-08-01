708694bb9ff7e8eb03b24fa382a80110a13a82b0
Emilie Gonin <emilie.yuu@gmail.com>
2023-08-01 15:32:57 +0200

    :rainbow: Fix build errors

--- Assets/UpgradeGear.cs
+++ Assets/UpgradeGear.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using InfinityCode.UltimateEditorEnhancer.Windows;
 using UnityEngine;
 
 public class UpgradeGear : MonoBehaviour
