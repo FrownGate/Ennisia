e8c14fdfd0115bf597cac494d2da224d082681fb
Jun <jzhi@gaming.tech>
2023-08-10 09:39:48 +0200

    :sparkles: Refactor Update method logic

--- Assets/Scripts/Battle/HUD/EffectHUD.cs
+++ Assets/Scripts/Battle/HUD/EffectHUD.cs
@@ -3,17 +3,20 @@
     using System.Collections.Generic;
     using UnityEngine;
     using UnityEngine.UIElements;
+    using Image = UnityEngine.UI.Image;
+    using Random = UnityEngine.Random;
 
     public class EffectHUD : MonoBehaviour
     {
         [SerializeField] private GameObject _container;
         [SerializeField] private GameObject _effectIconPrefab;
-        [SerializeField] private Image _effectIcon;
         [SerializeField] private Sprite[] _sprites;
 
         private Entity _entity { get; set; }
         private List<GameObject> _effectIcons = new();
+        private Image _effectIcon;
         public List<String> Effects { get; set; } = new();
+        private HashSet<string> _processedEffects = new HashSet<string>();
 
         private void Start()
         {
@@ -22,14 +25,13 @@
 
         private void Update()
         {
-            if (_entity.Effects.Count != _effectIcons.Count)
+            foreach (var effect in _entity.Effects)
             {
-                foreach (var effect in _entity.Effects)
+                // If it's a new effect we haven't processed yet
+                if (Effects.Contains(effect.Data.Name) && !_processedEffects.Contains(effect.Data.Name))
                 {
-                    if (Effects.Contains(effect.Data.Name) && !_effectIcons.Exists(x => x.name == effect.Data.Name))
-                    {
-                        AddEffectIcon(effect.Data.Name);
-                    }
+                    AddEffectIcon(effect.Data.Name);
+                    _processedEffects.Add(effect.Data.Name);
                 }
             }
         }
@@ -45,7 +47,9 @@
             var effect = Instantiate(_effectIconPrefab, _container.transform);
             effect.transform.SetParent(_container.transform);
             effect.name = effectName;
-            effect.GetComponent<SpriteRenderer>().sprite = _sprites[Effects.IndexOf(effectName)];
+            _effectIcon = effect.GetComponent<Image>();
+            _effectIcon.sprite = _sprites[Random.Range(0,3)];
+            Debug.LogWarning("Created Effect Icon");
             //_effectIcons.Add(effect);
         }
         
