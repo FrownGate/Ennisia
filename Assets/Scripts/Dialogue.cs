f6055c7ae495c05ec156072a9d4f8c368b22cbea
Valentin Faguet <valentin@wallmatt.com>
2023-08-24 17:42:06 +0200

    :star: Add Sprites to DialogueSystem
     NOT FINISHED

--- Assets/Scripts/Dialogue.cs
+++ Assets/Scripts/Dialogue.cs
@@ -1,21 +1,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Dialogue 
+public class Dialogue
 {
     public List<string> name;
     public List<string> dialogues;
+    public Dictionary<string, Sprite> sprites;
 
     public Dialogue(int id)
     {
         name = new();
         dialogues = new();
+        sprites = new();
+
         ReadCSVFile(id);
+        // LoadSprites(id);
     }
 
     public void ReadCSVFile(int id)
     {
-        string path = Application.dataPath + $"/Resources/CSV/DialogueSystem_{id}.csv";
+        string path = Application.dataPath + $"/Resources/CSV/Dialogue/DialogueSystem_{id}.csv";
         string[] lines = System.IO.File.ReadAllLines(path);
 
         if (lines.Length <= 1)
@@ -24,11 +28,11 @@ public class Dialogue
             return;
         }
 
-        string[] headers = lines[0].Split(',');
+        string[] headers = lines[0].Split(';');
 
         for (int i = 1; i < lines.Length; i++)
         {
-            string[] values = CSVUtils.SplitCSVLine(lines[i]);
+            string[] values = lines[i].Split(';');
 
             if (values.Length != headers.Length)
             {
